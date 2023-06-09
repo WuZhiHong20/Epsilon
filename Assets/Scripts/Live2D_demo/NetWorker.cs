69e545e41661fdd6f77cad3e69820aa87e9e8f48
wuzhihong <1228358476@qq.com>
2023-06-09 17:01:10 +0800

    噪音噪音噪音！
    得到了发送源的channels和frequencies，但是还是有噪音

--- Assets/Scripts/Live2D_demo/NetWorker.cs
+++ Assets/Scripts/Live2D_demo/NetWorker.cs
@@ -21,28 +21,31 @@ class NetWorker : MonoBehaviour
     string recvStr = "";
 
     List<Socket> checkList = new List<Socket>();
+
+    public AudioSource audioSource;
+
     private void Start()
     {
-        StartSymbol = false; fragmentNum= 0; AudioBuffer.Clear(); totalAudioSize= 0;
+        //StartSymbol = false; fragmentNum= 0; AudioBuffer.Clear(); totalAudioSize= 0;
         Logger.Init();
         Connection();
     }
 
-    private int fragmentSize = 4096;
-    private bool StartSymbol;
-    private int fragmentNum;
-    private int totalAudioSize;
+    private int fragmentSize = 2048;
+    //private bool StartSymbol;
+    //private int fragmentNum;
+    //private int totalAudioSize;
 
-    void StartRecvAudio(string recvStr)
-    {
-        string[] strings = recvStr.Split();
-        if(strings.Length == 3 ) { 
-            fragmentNum = int.Parse(strings[1]); StartSymbol = true; totalAudioSize = int.Parse(strings[2]);
-            Logger.Log($"{fragmentNum} {StartSymbol} {totalAudioSize}\n"); 
-        }
-    }
+    //void StartRecvAudio(string recvStr)
+    //{
+    //    string[] strings = recvStr.Split();
+    //    if(strings.Length == 3 ) { 
+    //        fragmentNum = int.Parse(strings[1]); StartSymbol = true; totalAudioSize = int.Parse(strings[2]);
+    //        Logger.Log($"{fragmentNum} {StartSymbol} {totalAudioSize}\n"); 
+    //    }
+    //}
 
-    //片段长是 4096
+    //片段长是 1024
     void RecvAudio(Socket s)
     {
         // 创建缓冲区用于接收数据
@@ -52,47 +55,71 @@ class NetWorker : MonoBehaviour
         // 处理接收到的数据
         if (bytesRead > 0)
         {
+            Logger.Log($"接收到{bytesRead}个bytes");
             //按需存储，防止存储片段之间有大量的000000000
             byte[] fragment = new byte[bytesRead];
             Array.Copy(buffer, fragment, bytesRead);
             // 将接收到的片段添加到列表中
             AudioBuffer.Add(fragment);
-            Logger.Log(Encoding.UTF8.GetString(fragment));
+            // Logger.Log(Encoding.UTF8.GetString(fragment));
         }
         else
         {
             Logger.Log($"Recv NO.{AudioBuffer.Count + 1} Fragment ERROR");
         }
-        if(AudioBuffer.Count == fragmentNum) { StartSymbol= false; Logger.Log($"StartSymbol change to false "); }
+        //if(AudioBuffer.Count == fragmentNum) { StartSymbol= false; Logger.Log($"StartSymbol change to false "); }
     }
 
-    void EndRecvAudio()
+    //void EndRecvAudio()
+    //{
+    //    // 合并音频片段
+    //    byte[] audioData = new byte[totalAudioSize];
+    //    int offset = 0;
+    //    foreach (byte[] fragment in AudioBuffer)
+    //    {
+    //        Array.Copy(fragment, 0, audioData, offset, fragment.Length);
+    //        offset += fragment.Length;
+    //    }
+    //    fragmentNum = 0 ; totalAudioSize = 0;
+    //    Logger.Log("Recv Over! Start Synhthesis");
+    //    SynhthesisAudio(audioData);
+    //}
+
+    private void SynhthesisAudio()
     {
-        // 合并音频片段
-        byte[] audioData = new byte[totalAudioSize];
-        int offset = 0;
-        foreach (byte[] fragment in AudioBuffer)
+        if(AudioBuffer.Count > 0)
         {
-            Array.Copy(fragment, 0, audioData, offset, fragment.Length);
-            offset += fragment.Length;
+            Debug.Log("Start Synhthesis");
+            OnSynhthesisAudio(AudioBuffer[0]);
+            AudioBuffer.RemoveAt(0);
         }
-        fragmentNum = 0 ; totalAudioSize = 0;
-        Logger.Log("Recv Over! Start Synhthesis");
-        SynhthesisAudio(audioData);
     }
 
-    private void SynhthesisAudio(byte[] audioData)
+    private void OnSynhthesisAudio(byte[] audioData)
     {
         // 创建空的 AudioClip
-        AudioClip audioClip = AudioClip.Create("ReceivedAudio", audioData.Length / 2, 1, 44100, false);
+        //AudioClip audioClip = AudioClip.Create("ReceivedAudio", audioData.Length / 2, 1, 44100, false);
+        AudioClip audioClip = AudioClip.Create("ReceivedAudio", audioData.Length / 2, 1, 22050, false);
+        //AudioClip audioClip = AudioClip.Create("ReceivedAudio", audioData.Length / 2, 1, 16000, false);
+        //AudioClip audioClip = AudioClip.Create("ReceivedAudio", audioData.Length / 2, 1, 65535, false);
 
         // 设置音频数据
         audioClip.SetData(ConvertBytesToFloats(audioData), 0);
 
+        Logger.Log("Synhthesis OK");
+        Debug.Log("Start Synhthesis");
         // 播放音频
-        AudioSource audioSource = GetComponent<AudioSource>();
+        StartCoroutine(PlayAudio(audioClip));
+    }
+
+    private IEnumerator PlayAudio(AudioClip audioClip)
+    {
         audioSource.clip = audioClip;
         audioSource.Play();
+        Logger.Log("Start Play");
+        float playTime = audioClip.length;
+        Logger.Log($"音频的时长是{playTime}s");
+        return new WaitForSecondsRealtime(playTime);
     }
 
     private float[] ConvertBytesToFloats(byte[] bytes)
@@ -102,7 +129,7 @@ class NetWorker : MonoBehaviour
         for (int i = 0, j = 0; i < bytes.Length; i += 2, j++)
         {
             short value = (short)(bytes[i] | (bytes[i + 1] << 8));
-            floats[j] = value / 32768f; // 将 16 位有符号整数转换为范围在 -1 到 1 之间的浮点数
+            floats[j] = value / 32768.0f; // 将 16 位有符号整数转换为范围在 -1 到 1 之间的浮点数
         }
 
         return floats;
@@ -119,29 +146,30 @@ class NetWorker : MonoBehaviour
 
         foreach(Socket s in checkList)
         {
-            if (StartSymbol == true)
-            {
-                RecvAudio(s);
-            }
-            byte[] readBuff = new byte[1024];
-            int count = s.Receive(readBuff);
-            //count > 10 不合理
-            if(count > 10) { Logger.Log($"长度不对劲！！！{count}" ); break; }
+            RecvAudio(s);
+            //if (StartSymbol == true)
+            //{
+            //    RecvAudio(s);
+            //}
+            //byte[] readBuff = new byte[1024];
+            //int count = s.Receive(readBuff);
+            ////count > 10 不合理
+            //if(count > 10) { Logger.Log($"长度不对劲！！！{count}" ); break; }
 
-            string recvStr = Encoding.Default.GetString(readBuff, 0, count);
-            if (recvStr.Contains("Start"))
-            {
-                StartRecvAudio(recvStr);
-                Logger.Log("Start Start Receive\n");
-            }
-            else if(recvStr.Contains("Over"))
-            {
-                EndRecvAudio();
-            }
+            //string recvStr = Encoding.Default.GetString(readBuff, 0, count);
+            //if (recvStr.Contains("Start"))
+            //{
+            //    StartRecvAudio(recvStr);
+            //    Logger.Log("Start Start Receive\n");
+            //}
+            //else if(recvStr.Contains("Over"))
+            //{
+            //    EndRecvAudio();
+            //}
             //Debug.Log(count);
             //Debug.Log(recvStr);
             //Logger.Log(recvStr);
-            inputField.text = recvStr;
+            //inputField.text = recvStr;
         }
     }
 
@@ -164,6 +192,7 @@ class NetWorker : MonoBehaviour
     private void Update()
     {
         UpdateNet();
+        SynhthesisAudio();
     }
 
     public void Connection()
---
+++ Assets/Scripts/Live2D_demo/test_WebRequest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class MyBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(GetAudioClip());
+    }
+
+    IEnumerator GetAudioClip()
+    {
+        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("https://www.my-server.com/audio.ogg", AudioType.OGGVORBIS))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                AudioClip myClip = DownloadHandlerAudioClip.GetContent(www);
+            }
+        }
+    }
+}
